fa80c2064ec5e50d25a880e98bc87f4fb4bdba5c
Simon Liang <no.lhr0909@gmail.com>
2012-06-12 10:36:59 -0400

    Initial commit for the files.

---
+++ EnhancedSharpSSH/SharpSSH-1.1.1.13.src/SharpSSHTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using Tamir.SharpSsh;
+
+
+namespace SharepSSHTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sftp sftp = new Sftp("10.5.1.108", "developer", "pandora");
+            //sftp.AddIdentityFile("[Path to Private kEY]");
+            sftp.Connect();
+            //sftp.Put(@"D:\temp\blog\feed.csv", "[PATH OF FILE ON SERVER]");
+            //sftp.Delete("[PATH OF FILE ON SERVER]");
+            ArrayList list = sftp.GetFileList("./a");
+            sftp.Close();
+        }
+    }
+}
